67454d6618d5da228d86fb55302cb8b9b85264ec
Andrew White <andrewh@cdw.com>
2026-07-27 07:30:59 -0500

    fix: experiment name omits min_wh in nested parameter sweep

--- run_mrc.sh
+++ run_mrc.sh
@@ -23,6 +23,6 @@ min_whs=(1 2 3 4 5 6 7 8 9 10)
 
 for min_area in "${min_areas[@]}"; do
     for min_wh in "${min_whs[@]}"; do
-        $python src/mrc/mrc.py min_area=$min_area min_wh=$min_wh exp_name=mrc_marea_${min_area}
+        $python src/mrc/mrc.py min_area=$min_area min_wh=$min_wh exp_name=mrc_marea_${min_area}_min_wh_${min_wh}
     done
 done
