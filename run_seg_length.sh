6a1887c32bb5986611f2ba161ce8c59e840bab46
Andrew White <andrewh@cdw.com>
2026-07-27 07:31:21 -0500

    fix: experiment name unchanged across segment length sweep

--- run_seg_length.sh
+++ run_seg_length.sh
@@ -23,5 +23,6 @@ seg_lengths=(40 60)
 exp_name="seg_length"
 
 for seg_length in "${seg_lengths[@]}"; do
+    exp_name="seg_length_${seg_length}"
     $python src/diffopc.py opc.SEG_LENGTH=$seg_length opc.WeightPVBL2=0.5 logger.aim.experiment=$exp_name
 done
