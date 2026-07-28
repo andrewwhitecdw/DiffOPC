62acd5281ef33bf65bfc6b7889aad393a632b60b
Andrew White <andrewh@cdw.com>
2026-07-27 20:46:23 -0500

    fix: experiment name unchanged across iteration/step sweeps

--- run_opc_iter.sh
+++ run_opc_iter.sh
@@ -23,56 +23,56 @@ steps=(1 2 4 8)
 iters=(60 70 80 90)
 
 # Exp 1
-exp_name="iter_seg60"
 for iter in "${iters[@]}"; do
     sraf_iter=$((iter + 20))
     for step in "${steps[@]}"; do
+        exp_name="iter_seg60_iter_${iter}_step_${step}"
         $python src/diffopc.py solver.device_id=$device_id opc.SEG_LENGTH=60 opc.Iterations=$iter opc.IsInsertSRAF=False opc.StepSize=$step logger.aim.experiment=$exp_name
     done
 done
 
-exp_name="iter_seg60_sraf"
 for iter in "${iters[@]}"; do
     sraf_iter=$((iter + 20))
     for step in "${steps[@]}"; do
+        exp_name="iter_seg60_sraf_iter_${iter}_step_${step}"
         $python src/diffopc.py solver.device_id=$device_id opc.SEG_LENGTH=60 opc.Iterations=$iter opc.SRAF_ITERATIONS=$sraf_iter opc.StepSize=$step logger.aim.experiment=$exp_name
     done
 done
 
 
 # Exp 2
-exp_name="iter_seg80"
 for iter in "${iters[@]}"; do
     sraf_iter=$((iter + 20))
     for step in "${steps[@]}"; do
+        exp_name="iter_seg80_iter_${iter}_step_${step}"
         $python src/diffopc.py solver.device_id=$device_id opc.SEG_LENGTH=80 opc.Iterations=$iter opc.IsInsertSRAF=False opc.StepSize=$step logger.aim.experiment=$exp_name
     done
 done
 
-exp_name="iter_seg80_sraf"
 for iter in "${iters[@]}"; do
     sraf_iter=$((iter + 20))
     for step in "${steps[@]}"; do
+        exp_name="iter_seg80_sraf_iter_${iter}_step_${step}"
         $python src/diffopc.py solver.device_id=$device_id opc.SEG_LENGTH=80 opc.Iterations=$iter opc.SRAF_ITERATIONS=$sraf_iter opc.StepSize=$step logger.aim.experiment=$exp_name
     done
 done
 
 
 # Exp 3
-exp_name="iter_seg100"
 
 for iter in "${iters[@]}"; do
     sraf_iter=$((iter + 20))
     for step in "${steps[@]}"; do
+        exp_name="iter_seg100_iter_${iter}_step_${step}"
         $python src/diffopc.py solver.device_id=$device_id opc.SEG_LENGTH=100 opc.Iterations=$iter opc.IsInsertSRAF=False opc.StepSize=$step logger.aim.experiment=$exp_name
     done
 done
 
 
-exp_name="iter_seg100_sraf"
 for iter in "${iters[@]}"; do
     sraf_iter=$((iter + 20))
     for step in "${steps[@]}"; do
+        exp_name="iter_seg100_sraf_iter_${iter}_step_${step}"
         $python src/diffopc.py solver.device_id=$device_id opc.SEG_LENGTH=100 opc.Iterations=$iter opc.SRAF_ITERATIONS=$sraf_iter opc.StepSize=$step logger.aim.experiment=$exp_name
     done
 done
