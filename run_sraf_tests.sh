77403efd99e0f7e15f1b90439580fb0bf859db01
Andrew White <andrewh@cdw.com>
2026-07-27 20:46:20 -0500

    fix: experiment names unchanged across SRAF parameter sweeps

--- run_sraf_tests.sh
+++ run_sraf_tests.sh
@@ -21,6 +21,7 @@ python=/home/local/eda13/gc29434/miniconda3/envs/dopc/bin/python
 thres_mins=(0.1 0.2 0.3 0.4)
 exp_name="thres_min"
 for thres_min in "${thres_mins[@]}"; do
+    exp_name="thres_min_${thres_min}"
     $python src/diffopc.py opc.SRAF_threshold_min=$thres_min logger.aim.experiment=$exp_name
 done
 
@@ -29,6 +30,7 @@ done
 forbiddens=(10 20 40 60)
 exp_name="sraf_forbidden"
 for forbidden in "${forbiddens[@]}"; do
+    exp_name="sraf_forbidden_${forbidden}"
     $python src/diffopc.py opc.SRAF_FORBIDDEN=$forbidden logger.aim.experiment=$exp_name
 done
 
@@ -36,11 +38,13 @@ done
 areas=(300 400 600 900)
 exp_name="sraf_area"
 for area in "${areas[@]}"; do
+    exp_name="sraf_area_${area}"
     $python src/diffopc.py opc.SRAF_contour_area=$area logger.aim.experiment=$exp_name
 done
 
 initial_sraf_whs=(20 40 60 80)
 exp_name="init_sraf_wh"
 for wh in "${initial_sraf_whs[@]}"; do
+    exp_name="init_sraf_wh_${wh}"
     $python src/diffopc.py opc.SRAF_initial_sraf_wh=$wh logger.aim.experiment=$exp_name
 done
